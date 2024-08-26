547edc137b6f78de23a182cee31cba3af4cac06e
Genis Muner <munergenis@gmail.com>
2024-08-26 18:04:53 +0200

    add feature delete task in category

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,52 @@ import Layout from 'layout/index'
 import TaskList from 'components/TaskList/index'
 import taskListsData from 'src/data/taskListsData'
 import { useState } from 'react'
+import { useEffect } from 'react'
 
 const App = () => {
-  const [selectedCategory, setSelectedCategory] = useState(taskListsData.length ? taskListsData[0] : null)
+  const [allData, setAllData] = useState(taskListsData)
+  const [selectedCategory, setSelectedCategory] = useState(
+    allData.length
+      ? allData[0]
+      : null
+  )
+
+  useEffect(() => {
+    setSelectedCategory(prevSelectedCategory => {
+      return allData.filter(category => {
+        console.log(category.id)
+        console.log(prevSelectedCategory.id)
+        console.log(prevSelectedCategory.id === category.id)
+        return category.id === prevSelectedCategory.id
+      })[0]
+    })
+  }, [allData])
 
   function getTaskListElements (selectedCategory) {
-    return selectedCategory.taskList.map(taskItem => (
-      <TaskList.Item
-        key={taskItem.id}
-        completed={taskItem.completed}
-      >
-        {taskItem.task}
-      </TaskList.Item>
-    ))
+    return selectedCategory.taskList.map(
+      taskItem => (
+        <TaskList.Item
+          key={taskItem.id}
+          completed={taskItem.completed}
+          deleteTask={() => deleteTask(taskItem.id, selectedCategory.id)}
+        >
+          {taskItem.task}
+        </TaskList.Item>
+      )
+    )
+  }
+
+  function deleteTask (taskID, categoryID) {
+    setAllData(prevData =>
+      prevData.map(category =>
+        category.id !== categoryID
+          ? category
+          : {
+              ...category,
+              taskList: category.taskList.filter(task => task.id !== taskID)
+            }
+      )
+    )
   }
 
   return (
